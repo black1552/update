c73fee96821cb46a3c48f54f70cd03c785c88d3b
清风￡流云 <black1552@163.com>
2021-05-26 11:08:26 +0800

    添加基本操作说明

--- app/src/main/java/com/liuchang/update/MainActivity.kt
+++ app/src/main/java/com/liuchang/update/MainActivity.kt
@@ -2,10 +2,36 @@ package com.liuchang.update
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.liuchang.http.ApiManager
+
+/**
+ * 添加所有需要用到的引用库
+ * 包含：
+ *      通用适配器
+ *      基础网络通信组件
+ *      图片加载组件
+ *      各种工具类
+ *      应用更新
+ * 使用方式代码中使用
+ **/
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 更新
+        Updateinit.context = this.application
+        //  更新效验地址
+        Updateinit.url = ""
+        Updateinit.initXAOP()
+        Updateinit.initOKHttpUtils()
+        Updateinit.initUpdate()
+
+        // 网络请求基地址
+        ApiManager.base = ""
+        // 网络请求基础及地址
+        ApiManager.baseUrl = ""
+        // 其他如以往添加实体及请求方法
     }
 }
